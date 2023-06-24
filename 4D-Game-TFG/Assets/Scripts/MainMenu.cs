77c0ab8665e5d9c28bed85962fbb1da2ee43ac45
Osvak <61042986+Osvak@users.noreply.github.com>
2023-06-25 01:23:59 +0200

    done

--- 4D-Game-TFG/Assets/Scripts/MainMenu.cs
+++ 4D-Game-TFG/Assets/Scripts/MainMenu.cs
@@ -1,12 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] public GameObject mainMenu;
+
     public bool isControlsEnable = false;
     [SerializeField] public GameObject controlsMenu;
+
+    public bool isVideoPlaying = false;
+    [SerializeField] public VideoPlayer videoPlayer;
+    [SerializeField] public GameObject rawImage;
+
+    [SerializeField] public GameObject audioPlayer; 
+
+    public GameObject title;
+
+    private void Start()
+    {
+        videoPlayer.Stop();
+        videoPlayer.Prepare();
+    }
+
+    private void Update()
+    {
+
+        StopVideo();
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
@@ -22,13 +47,46 @@ public class MainMenu : MonoBehaviour
         {
             isControlsEnable = true;
             controlsMenu.SetActive(true);
-            gameObject.SetActive(false);
+            mainMenu.SetActive(false);
         }
         else
         {
             isControlsEnable = false;
             controlsMenu.SetActive(false);
-            gameObject.SetActive(true);
+            mainMenu.SetActive(true);
+        }
+    }
+
+    public void PlayVideo()
+    {
+        if (isVideoPlaying == false)
+        {
+            isVideoPlaying = true;
+            rawImage.SetActive(true);
+            mainMenu.SetActive(false);
+            title.SetActive(false);
+            audioPlayer.GetComponent<AudioSource>().Pause();
+
+            videoPlayer.Play();
+
+        }
+    }
+
+    public void StopVideo()
+    {
+        if (isVideoPlaying == true && (videoPlayer.frame >= (long)videoPlayer.frameCount - 1))
+        {
+
+            Debug.Log("Video ended");
+
+            isVideoPlaying = false;
+            rawImage.SetActive(false);
+            mainMenu.SetActive(true);
+            title.SetActive(true);
+
+            audioPlayer.GetComponent<AudioSource>().UnPause();
+
+            videoPlayer.Stop();
         }
     }
 }
